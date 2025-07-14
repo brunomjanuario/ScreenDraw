e521a2c188fc1bd32629395ee89a8de3e17e841e
Bruno Januário <bruno.januario1998@gmail.com>
2025-07-14 21:34:07 +0100

    Initial Commit

---
+++ ScreenDraw/ScreenDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenDrawApp.swift
+//  ScreenDraw
+//
+//  Created by Bruno Januário on 14/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScreenDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
